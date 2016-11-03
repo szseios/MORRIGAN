50e009cb2c57d0d4c4adea563ede2b0324c566c5
mac-jhw <hwju@assur.local>
2016-11-03 20:43:06 +0800

    自动按摩界面功能ok

--- MORRIGAN/UI/Login/View/FuntionButton.h
+++ MORRIGAN/UI/Login/View/FuntionButton.h
@@ -13,6 +13,7 @@
 @property(nonatomic,strong)UIImage *buttonImage;             // 按钮的图标
 @property(nonatomic,assign)CGFloat distanceFromDragButton;   // 计算的与拖动按钮的距离
 @property(nonatomic,assign)NSInteger arrayIndex;             // 所在数组下标
+@property(nonatomic,strong)NSString *funCodeString;           // 对应的指令串
 
 
 -(instancetype)initWithFrame:(CGRect)frame;
