bd3856b84ef411e03e3e65a282ee11f4962df321
snhuang <1989215@qq.com>
2016-11-07 19:57:44 +0800

    添加fmdb

---
+++ MORRIGAN/Common/DBManager/DBManager.h
@@ -0,0 +1,13 @@
+//
+//  DBManager.h
+//  MORRIGAN
+//
+//  Created by snhuang on 2016/11/7.
+//  Copyright © 2016年 mac-jhw. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DBManager : NSObject
+
+@end
